93b5ded49b16fe47d707997a048050d4288d4650
Srija Paul <115783620+srijapaul@users.noreply.github.com>
2025-05-02 11:20:09 +0530

    Update cypress.config.ts

--- frontend/my-next-app/cypress/cypress.config.ts
+++ frontend/my-next-app/cypress/cypress.config.ts
@@ -1,9 +1,1 @@
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000',
-    screenshotOnRunFailure: true,
-    video: true,
-  },
-});
+//
